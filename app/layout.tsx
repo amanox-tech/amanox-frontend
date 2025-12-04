d3287075071386e8262f5dcf0e36f35aae59aa7d
Arfat Sayyed <sdarfat77@gmail.com>
2025-12-04 22:52:27 +0530

    Logo Added and Toast Notification Improved

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import "./globals.css";
 import { AppProvider } from "@/context/appContext";
 import { ReactNode } from "react";
-import { ToastContainer } from "react-toastify";
+// 1. IMPORT REACT HOT TOAST
+import { Toaster } from "react-hot-toast";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 
@@ -11,11 +12,37 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <body>
         <AppProvider>
           <Navbar />
-          <main className="min-h-screen pt-20 pb-20">{children}</main>
+          <main className="min-h-screen relative">{children}</main>
           <Footer />
         </AppProvider>
 
-        <ToastContainer />
+        <Toaster
+          position="top-right"
+          reverseOrder={false}
+          toastOptions={{
+            duration: 4000,
+            className:
+              "!bg-white/80 !backdrop-blur-lg !border !border-white/60 !shadow-[0_20px_40px_-10px_rgba(0,0,0,0.1)] !rounded-2xl !px-5 !py-4 !text-sm !font-semibold !text-secondary !font-sans",
+
+            style: {
+              color: "#373643",
+            },
+
+            success: {
+              iconTheme: {
+                primary: "#18cb96",
+                secondary: "white",
+              },
+            },
+
+            error: {
+              iconTheme: {
+                primary: "#EF4444",
+                secondary: "white",
+              },
+            },
+          }}
+        />
       </body>
     </html>
   );
